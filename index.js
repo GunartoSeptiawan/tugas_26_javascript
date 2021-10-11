023be8c5a3ae0be45f288e97bf05b7a0d11d4ba9
Gunarto Septiawan <gunartosept@gmail.com>
2021-10-11 16:24:09 +0700

    tugas_26_javascript
    Tugas Niomic

---
+++ index.js
@@ -0,0 +1,60 @@
+function panggilIsFinite() {
+  var angka = [2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16];
+
+  var a = 2;
+  var b = 2 / 0;
+  var a = "Angka 2";
+  console.log(a, b);
+
+  var c = 39;
+  var d = 39 / 0;
+  var c = "Angka 39";
+  console.log(c, d);
+
+  var e = 76;
+  var f = 76 / 0;
+  var e = "Angka 76";
+  console.log(e, f);
+
+  var g = 50;
+  var h = 50 / 0;
+  var g = "Angka 50";
+  console.log(g, h);
+
+  var i = 9;
+  var j = 9 / 0;
+  var i = "Angka 9";
+  console.log(i, j);
+
+  var k = 7;
+  var l = 7 / 0;
+  var k = "Angka 7";
+  console.log(k, l);
+
+  var m = 41;
+  var n = 41 / 0;
+  var m = "Angka 41";
+  console.log(m, n);
+
+  var o = 2;
+  var p = 2 / 0;
+  var o = "Angka 2";
+  console.log(o, p);
+
+  var q = 24;
+  var r = 24 / 0;
+  var q = "Angka 24";
+  console.log(q, r);
+
+  var s = 1;
+  var t = 1 / 0;
+  var s = "Angka 1";
+  console.log(s, t);
+
+  var u = 16;
+  var v = 16 / 0;
+  var u = "Angka 16";
+  console.log(u, v);
+}
+
+panggilIsFinite();
